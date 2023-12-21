98f96dd10f8dd80f7080e22d53769a4d5d2a90eb
DezzyCodes <desmondslatterypersonal@gmail.com>
2023-12-21 13:48:59 +0000

    currency conversion now uses an API
    web scraping has now been replaced by Free ExchangeRate API

--- project/tools/currency_converter.py
+++ project/tools/currency_converter.py
@@ -1,28 +1,20 @@
-from bs4 import BeautifulSoup
 import requests 
+import os
 from decimal import Decimal
 
 
-def perform_exchange(amount: float, from_symbol: str, to_symbol: str):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
-    url = f'https://finance.yahoo.com/quote/{from_symbol}{to_symbol}=X/'
+def perform_exchange(from_symbol: str, to_symbol: str, amount: float):
+    key = os.getenv("EXC_API_KEY")
+    url = f"https://v6.exchangerate-api.com/v6/{key}/pair/{from_symbol}/{to_symbol}"
 
-    source = requests.get(url=url, headers=headers).text
-    soup = BeautifulSoup(source, 'lxml')
-
-    # if from symbol is USD set to empty string
-    # rationale: data-symbol on Yahoo Finance just has 'to_currency'=X
-    if from_symbol == "USD":
-        from_symbol = ""
-
-    element = soup.find(
-        attrs={
-            'data-field': 'regularMarketPrice', 
-            'data-symbol': f'{from_symbol}{to_symbol}=X'
-        }
-    )
-
-    value = Decimal(element.get("value")) if element else Decimal('0.0')
-    result = (amount * value).quantize(Decimal('0.01'))
+    conv_rate = Decimal(requests.get(url).json()["conversion_rate"])
+    result = (amount * conv_rate).quantize(Decimal("0.01"))
     return result
+
+
+
+    
+
+
+
 
